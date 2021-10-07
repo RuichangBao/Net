248e6aa31d18d2fa5f7d8fdfbdfcaf51f4618f45
RuichangBao <1294838346@qq.com>
2021-10-07 19:11:26 +0800

    服务器开发

---
+++ Second/Client/Assets/Scripts/Init.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ = NetManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Second/Client/Assets/Scripts/NetManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+public class NetManager
+{
+    private static NetManager instance;
+    public static NetManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new NetManager();
+            return instance;
+        }
+    }
+    private NetManager()
+    {
+        Debug.Log("NetNanager");
+        UdpClient udpClient = new UdpClient(8818);//端口号
+        //udpClient.
+  } }
