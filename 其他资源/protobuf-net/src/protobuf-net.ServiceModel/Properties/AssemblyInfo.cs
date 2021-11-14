5154166521814860be29bd3cd9eb939f487e1b6a
RuichangBao <1294838346@qq.com>
2021-11-14 10:18:15 +0800

    删除资源

--- 其他资源/protobuf-net/src/protobuf-net.ServiceModel/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿
-using System.Runtime.CompilerServices;
-
-#if PLAT_SKIP_LOCALS_INIT
-[module: SkipLocalsInit]
-#endif
--- 其他资源/protobuf-net/src/protobuf-net/Internal/Serializers/IDirectWriteNode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProtoBuf.Internal.Serializers
-{
-    internal interface IDirectWriteNode
-    {
-        bool CanEmitDirectWrite(WireType wireType);
-        void EmitDirectWrite(int fieldNumber, WireType wireType, Compiler.CompilerContext ctx, Compiler.Local valueFrom);
-    }
-}
