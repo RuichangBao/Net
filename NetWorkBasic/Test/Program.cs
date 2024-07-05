4189bdcdd8935e785cad7761fcf4825fcdd6bb18
RuichangBao <1294838346@qq.com>
2024-07-05 10:39:41 +0800

    协议剥离

--- NetWorkBasic/Test/Program.cs
+++ NetWorkBasic/Test/Program.cs
@@ -4,13 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Test1Request test1Request = new Test1Request
-            {
-                ResultPerPage = 10,
-                PageNumber = 1,
-                Query = "safdf"
-            };
-            Console.WriteLine(test1Request.ToString());
             Console.WriteLine("Hello, World!");
         }
     }
